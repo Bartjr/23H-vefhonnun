9b70194b76a7c7f163f0d0229f4f7d0d3cba3b5c
Bartjr <bjarturcasadei@gmail.com>
2023-11-29 16:09:01 +0000

    yup

--- website/javascript/aboutus.js
+++ website/javascript/aboutus.js
@@ -1,5 +1,22 @@
+function toggleTeamNames() {
+    const teamNames = document.getElementById('teamNames');
+    teamNames.style.display = (teamNames.style.display === 'block') ? 'none' : 'block';
+}
 
-function toggleTeamInfo(teamId) {
-    const teamInfo = document.getElementById(teamId + '-info');
-    teamInfo.classList.toggle('active');
+function toggleMemberInfo(memberName) {
+    const memberInfo = document.getElementById(`${memberName}Info`);
+    memberInfo.classList.toggle('active');
+}
+
+function showMemberInfo(memberName) {
+    const memberInfo = document.getElementById(`${memberName}Info`);
+
+
+    // Slide out animation
+    memberInfo.style.height = 'auto';
+    const height = memberInfo.clientHeight + 'px';
+    memberInfo.style.height = '0';
+    setTimeout(() => {
+        memberInfo.style.height = height;
+    }, 0);
 }
